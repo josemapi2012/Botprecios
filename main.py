550114584a23c1c7ed47f0f15d6a5b8079ef88e3
josemapi2012 <josemapi2012@gmail.com>
2020-11-09 22:32:25 +0100

    added while loop and time to main

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from Footlocker import HighMocha, CourtPurple, sbDunkHigh ,sbDunkLow
 from Amazon  import redmi9, switch
 from supreme import AirForce
-#Amazon
-redmi9()
-switch()
-#Footlocker
-HighMocha()
-CourtPurple()
-sbDunkLow()
-sbDunkHigh()
-#supreme
-AirForce()
+import time
+
+while TRUE:
+    time.sleep(300)
+    #Amazon
+    redmi9()
+    switch()
+    #Footlocker
+    HighMocha()
+    CourtPurple()
+    sbDunkLow()
+    sbDunkHigh()
+    #supreme
+    AirForce()
