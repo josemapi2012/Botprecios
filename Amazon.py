62bf849f801bda4045cb390b79caef9afc2d92e5
josemapi2012 <josemapi2012@gmail.com>
2020-11-15 12:17:41 +0100

    Remove redmi9 track

--- Amazon.py
+++ Amazon.py
@@ -5,42 +5,16 @@ import datetime
 
 headers = {"User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36 Edg/86.0.622.63'}
 
-def redmi9():
-    page = requests.get('https://www.amazon.es/Xiaomi-Tel%C3%A9fono-Pantalla-Octa-Core-Procesador/dp/B08B1GVJ9J/ref=sr_1_2?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=redmi+9&qid=1604832504&quartzVehicle=93-1829&replacementKeywords=redmi&sr=8-2', headers=headers)
-
-    soup = BeautifulSoup(page.content, 'lxml')
-    try:
-        price = soup.find(id="priceblock_ourprice").get_text()
-        converted_price =(price[0:3])
-    except:
-        x = datetime.datetime.now()
-        f = open("log.txt","a")
-        f.write(f"{x} - Error al obtener el precio del redmi 9 \n")
-        f.close()
-    
-    if (int(converted_price) < 146):
-    	os.system(f"telegram-send 'El precio del movil es '{converted_price}")
-    else:
-            x = datetime.datetime.now()
-            f = open("log.txt","a")
-            f.write(f"{x} - El redmi 9 todavia no ha bajado de precio \n")
-            f.close()
 
 def switch():
     page = requests.get('https://www.amazon.es/Nintendo-Switch-Consola-color-Modelo/dp/B07W13KJZC/ref=sr_1_3?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=nintendo+switch&qid=1604946320&sr=8-3https://www.amazon.es/Nintendo-Switch-Consola-color-Modelo/dp/B07W13KJZC/ref=sr_1_3?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=nintendo+switch&qid=1604946320&sr=8-3', headers=headers)
 
     soup = BeautifulSoup(page.content, 'lxml')
-    try:
-        price = soup.find(id="priceblock_ourprice").get_text()
-        converted_price =(price[0:3])
-    except:
-        x = datetime.datetime.now()
-        f = open("log.txt","a")
-        f.write(f"{x} - Error al obtener el precio de la switch \n")
-        f.close()
+
+    price = soup.find(id="priceblock_ourprice").get_text()
+    converted_price =(price[0:3])
     
-    
-    if (int(converted_price) < 146):
+    if (int(converted_price) < 296):
     	os.system(f"telegram-send 'El precio de la nintendo switch es  '{converted_price}")
     else:
             x = datetime.datetime.now()
